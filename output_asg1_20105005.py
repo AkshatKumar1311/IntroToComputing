ad3b34d7122fe8eaf598be331b516c4994f77c10
AkshatKumar1311 <97615763+AkshatKumar1311@users.noreply.github.com>
2022-01-19 00:05:24 +0530

    Add files via upload

---
+++ output_asg1_20105005.py
@@ -0,0 +1,46 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: C:\Users\Akshat\OneDrive\Desktop\asg1_20105005_Akshat.py ======
+Q1  
+
+Enter number 1 23
+Enter number 2 57
+Enter number 3 99
+Sum of given numbers is  179
+Average of given numbers is  59.666666666666664 
+
+Q2  
+
+All values are to be entered in dollars
+Enter your gross income $60000
+Enter number of dependents 3
+The tax comes out to be $ 8200.0 
+
+Q3  
+
+Student = [sid, name, gender, course, cgpa]
+Enter your SID 20105005
+Enter your name Akshat Kumar
+Enter your gender 'M', 'F', 'U' for Male, Female, Unknown M
+Enter your course name Mathematics
+Enter your cgpa 7.4
+Student info  [20105005, 'Akshat Kumar', 'M', 'Mathematics', 7.4] 
+
+Q4 
+
+Enter marks of student 1 95
+Enter marks of student 2 89
+Enter marks of student 3 62
+Enter marks of student 4 84
+Enter marks of student 5 91
+marks of 5 students are  [95, 89, 62, 84, 91]
+Marks in ascending order  [62, 84, 89, 91, 95]
+Marks in descending order  [95, 91, 89, 84, 62] 
+
+Q5 
+
+The provided list is  ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow'] 
+
+The first modified list  is color a = ['Red', 'Green', 'White', 'Pink', 'Yellow']
+The second modified list is color b = ['Red', 'Green', 'White', 'Purple', 'Yellow']
